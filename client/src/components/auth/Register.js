aa5f1bde449a76d3580bbf541d55a47e4ed46475
himanshukorpal <himanshukorpal@yahoo.com>
2021-02-09 16:02:43 +0530

    added new functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-
+import { Link } from "react-router-dom";
 const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -17,10 +17,10 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if (password != password2) {
+    if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      console.log("Sucess");
     }
   };
 
@@ -80,7 +80,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
